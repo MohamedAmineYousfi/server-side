a9eccf225bf71348b12767e8953464a0e9d7f9f9
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-09 11:23:02 +0100

    merge

--- serverwithpg/src/app.module.ts
+++ serverwithpg/src/app.module.ts
@@ -3,10 +3,20 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as Joi from 'joi';
-
+import { DatabaseModule } from './dataBase//database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    validationSchema: Joi.object({
+      POSTGRES_HOST: Joi.string().required(),
+      POSTGRES_PORT: Joi.number().required(),
+      POSTGRES_USER: Joi.string().required(),
+      POSTGRES_PASSWORD: Joi.string().required(),
+      POSTGRES_DB: Joi.string().required(),
+      PORT: Joi.number(),
+    }),
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
