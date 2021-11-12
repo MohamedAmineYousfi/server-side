c8025613843b676370f064fd6e409506c455ed69
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-12 21:22:23 +0100

    change blog

--- src/blogs/entities/blog.entity.ts
+++ src/blogs/entities/blog.entity.ts
@@ -2,26 +2,21 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Blog {
-    @PrimaryGeneratedColumn()
-    public id: number;
+  @PrimaryGeneratedColumn()
+  public id: number;
 
-    @Column()
-    public user_name: number
+  @Column()
+  public blogTitle: string;
 
-    @Column()
-    public blogTitle: string;
+  @Column()
+  public imageUrl: string;
 
-    @Column()
-    public imageUrl: string;
+  @Column({ type: 'varchar', length: 300 })
+  public content: string;
 
-    @Column({ type: 'varchar', length: 300 })
-    public content: string;
+  @Column()
+  public date: Date;
 
-    @Column()
-    public date: Date;
-   
-
-    @Column({ default :0 , nullable: true })
-    public like: number;
-
+  @Column({ default: 0, nullable: true })
+  public like: number;
 }
