f516748cb9d03f4d37765c306dea7072de73018d
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-12 15:30:22 +0100

    removed dist

--- dist/admin/dto/create-admin.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateAdminDto {
-    first_Name: string;
-    last_Name: string;
-    email: string;
-    password_hashed: string;
-    salt: string;
-    phone_Num: number;
-    createdAt: Date;
-}
