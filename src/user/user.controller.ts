62f6a80318738dcd4b4a82d398ecfe88eaf7d206
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-13 17:09:48 +0100

    change

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,7 +8,7 @@ import {
   Param,
   Delete,
   Res,
-  HttpStatus
+  HttpStatus,
 } from '@nestjs/common';
 
 import { UsersService } from './user.service';
@@ -17,67 +17,48 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 var jwt = require('jsonwebtoken');
-import { Response,response } from 'express';
+import { Response, response } from 'express';
 
 @Controller('users')
-
-
 export class UsersController {
-
-  constructor(private  usersService: UsersService) {}
-
+  constructor(private usersService: UsersService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto , @Res() response:Response) {
-  const res = await this.usersService.create(createUserDto);
-    const token = jwt.sign(
-      { user_id: res.id },
-      process.env.TOKEN_KEY
-    );}
-
-
+  async create(
+    @Body() createUserDto: CreateUserDto,
+    @Res() response: Response,
+  ) {
+    const res = await this.usersService.create(createUserDto);
+    const token = jwt.sign({ user_id: res.id }, process.env.TOKEN_KEY);
+  }
 
   @Post('login')
-  login(@Body() user: CreateUserDto, @Res() response:Response)  {
-     console.log(user)
-      var token = jwt.sign(
-      {id:user.id,email:user.email},
-      process.env.TOKEN_KEY
-    )
-        response.status(HttpStatus.CREATED)
-        .json({Token:token})
-      }
+  login(@Body() user: CreateUserDto, @Res() response: Response) {
+    console.log(user);
+    var token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.TOKEN_KEY,
+    );
+    response.status(HttpStatus.CREATED).json({ Token: token });
+  }
 
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
 
-      @Get()
-      findAll() {
-        return this.usersService.findAll();
-      }
-    
-      @Get(':id')
-      findOne(@Param('id') id: string) {
-        return this.usersService.findOne(+id);
-      }
-    
-      @Patch(':id')
-      update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return this.usersService.update(+id, updateUserDto);
-      }
-    
-      @Delete(':id')
-      remove(@Param('id') id: string) {
-        return this.usersService.remove(+id);
-      }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
 
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
 
-
-
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
 }
-
-  
-
-
-  
-
-
-
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,10 +5,8 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { map, switchMap} from 'rxjs/operators';
-import { Observable, of,from } from 'rxjs';
-
-
+import { map, switchMap } from 'rxjs/operators';
+import { Observable, of, from } from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -36,49 +34,43 @@ export class UsersService {
     await this.usersRepository.delete(id);
   }
 
-
   login(user: User) {
     return this.validateUser(user.email, user.password).pipe(
-        switchMap((user: User) => {
-            if(user) {
-                return this.authService.generateJWT(user).pipe(map((jwt: string) => jwt));
+      switchMap((user: User) => {
+        if (user) {
+          return this.authService
+            .generateJWT(user)
+            .pipe(map((jwt: string) => jwt));
+        } else {
+          return 'Wrong Credentials';
+        }
+      }),
+    );
+  }
+
+  validateUser(email: string, password: string) {
+    return from(
+      this.usersRepository.findOne(
+        { email },
+        { select: ['id', 'password', 'email'] },
+      ),
+    ).pipe(
+      switchMap((user: User) =>
+        this.authService.comparePasswords(password, user.password).pipe(
+          map((match: boolean) => {
+            if (match) {
+              const { password, ...result } = user;
+              return result;
             } else {
-                return 'Wrong Credentials';
+              throw Error;
             }
-        })
-    )
+          }),
+        ),
+      ),
+    );
+  }
+
+  findByMail(email: string): Observable<User> {
+    return from(this.usersRepository.findOne({ email }));
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-validateUser(email: string, password: string) {
-    return from(this.usersRepository.findOne({email}, {select: ['id', 'password',  'email']})).pipe(
-        switchMap((user: User) => this.authService.comparePasswords(password, user.password).pipe(
-            map((match: boolean) => {
-                if(match) {
-                    const {password, ...result} = user;
-                    return result;
-                } else {
-                    throw Error;
-                }
-            })
-        ))
-    )
-
-}
-
-findByMail(email: string): Observable<User> {
-  return from(this.usersRepository.findOne({email}));
-}
-
-}
-
