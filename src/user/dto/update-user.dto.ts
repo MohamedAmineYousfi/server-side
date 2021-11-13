db4653812a72bd66318c622a7590d139abd2eddd
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-13 16:47:47 +0100

    auth

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,60 +1,40 @@
 /* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import {Gym} from '../../gyms/entities/gym.entity'
-import {Event} from '../../events/entities/event.entity'
-import {Coach} from '../../coachs/entities/coach.entity'
+import { Gym } from '../../gyms/entities/gym.entity';
+import { Event } from '../../events/entities/event.entity';
+import { Coach } from '../../coachs/entities/coach.entity';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
- 
-    id: number
+  id: number;
 
+  imageUrl: string;
 
-    imageUrl: string
+  age: number;
 
+  first_name: string;
 
-    age: number
-    
-    
-    first_name: string 
+  last_name: string;
 
-    
-    last_name: string
+  phone_number: number;
 
-    
-    phone_number: number
+  email: string;
 
-    
-    email: string
+  password: string;
 
+  registred_at: Date;
 
-    password: string 
-     
- 
-    registred_at: Date 
+  gyms: Gym[];
 
+  coachs: Coach[];
 
-    gyms: Gym[] 
- 
-     
-    coachs: Coach[] 
+  events: Event[];
 
-   
-    events: Event[]
+  bmi: number;
 
-    
-    bmi: number
+  weight: number;
 
-    
-    weight: number
+  height: number;
 
-    
-    height: number
-
-
-    adress : string 
-
-    imageUrl : string 
-
+  adress: string;
 }
-
