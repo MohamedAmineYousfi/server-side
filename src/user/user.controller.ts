a68a914b3a975b9908108365ac76953c92a6c5f6
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-17 14:27:00 +0100

    change to ES6

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -20,11 +20,13 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto, @Res() response: Response) {
-    return this.usersService.create(createUserDto).then((res) => {
-      const token = jwt.sign({ user_id: res.id }, process.env.TOKEN_KEY);
-      response.status(HttpStatus.CREATED).json({ Token: token });
-    });
+  async create(
+    @Body() createUserDto: CreateUserDto,
+    @Res() response: Response,
+  ) {
+    const res = await this.usersService.create(createUserDto);
+    const token = jwt.sign({ user_id: res.id }, process.env.TOKEN_KEY);
+    response.status(HttpStatus.CREATED).json({ Token: token });
   }
 
   @Get()
